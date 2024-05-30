7085c48aa310c4cddefac07413bcc60a7eafe3ee
ushasri2645 <160113438+ushasri2645@users.noreply.github.com>
2024-05-30 11:20:55 +0530

    Update utils.js

--- Assignment5/utils.js
+++ Assignment5/utils.js
@@ -1,1 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Utils = void 0;
+var result2 = '';
+var Utils;
+(function (Utils) {
+    function PrintObjects(objs) {
+        for (var property in objs) {
+            if (typeof objs[property] == 'object') {
+                result2 += "".concat(PrintObjects(objs[property]), ", ");
+            }
+            else {
+                result2 += "".concat(property, " : ").concat(objs[property], ", ");
+            }
+        }
+        result2 = result2.slice(0, -2);
+        return result2;
+    }
+    Utils.PrintObjects = PrintObjects;
+})(Utils || (exports.Utils = Utils = {}));
 
