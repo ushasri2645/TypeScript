2568921dea2954c5c5c799af4fe96c4dee7e181b
ushasri2645 <160113438+ushasri2645@users.noreply.github.com>
2024-05-30 11:20:15 +0530

    Update utils.ts

--- Assignment5/utils.ts
+++ Assignment5/utils.ts
@@ -1,1 +1,15 @@
-
+let result2='';
+export namespace Utils{
+    export function PrintObjects(objs : any) : string{
+        for(const property in objs){
+            if(typeof objs[property] == 'object' ){
+                result2 += `${PrintObjects(objs[property])}, `;
+            }
+            else{
+                result2 += `${property} : ${objs[property]}, `;
+            }
+        }
+        result2=result2.slice(0,-2);
+        return result2;
+}
+}
